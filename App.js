1b555d45aa6549976a4b71adb3dffb5cdf802e91
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-03-25 08:51:06 -0300

    feat: instalação dos recursos para geo-localização

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
+  const regiaoInicialMapa = {
+    latitude: -10,
+    longitude: -55,
+
+    /* Definição do zoom do mapa.
+    Quanto menor, mais próximo o mapa. */
+    latitudeDelta: 40,
+    longitudeDelta: 40,
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <MapView style={styles.mapa} initialRegion={regiaoInicialMapa} />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1 },
+  mapa: { width: "100%", height: "100%" },
 });
