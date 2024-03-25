bee979959939e82f97eec60261d0ac966f5c808e
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-03-25 09:23:00 -0300

    feat: config do marker padrão

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
+import fantasma from "./assets/ghost.png";
 
 export default function App() {
+  /* Coordenadas para o MapView */
   const regiaoInicialMapa = {
+    /*
+    Coordenadas para o Brasil:
     latitude: -10,
-    longitude: -55,
+    longitude: -55, */
+    // São Paulo
+    latitude: -23.53,
+    longitude: -46.65529,
 
     /* Definição do zoom do mapa.
     Quanto menor, mais próximo o mapa.
@@ -13,6 +20,15 @@ export default function App() {
     latitudeDelta: 40,
     longitudeDelta: 40,
   };
+
+  /* Coordenadas para Marker que será aplicado ao MapView */
+  const localizacao = {
+    latitude: -33.867886,
+    longitude: -63.987,
+    latitudeDelta: 10,
+    longitudeDelta: 10,
+  };
+
   return (
     <>
       <StatusBar />
@@ -22,9 +38,11 @@ export default function App() {
           userInterfaceStyle="dark" // funcionara só para ios
           style={styles.mapa}
           initialRegion={regiaoInicialMapa}
-          minZoomLevel={5} // Delimitando o zoom minimo do usuário
-          maxZoomLevel={15} // Delimitando o zoom máximo do usuário
-        />
+          // minZoomLevel={5}  Delimitando o zoom minimo do usuário
+          // maxZoomLevel={15}  Delimitando o zoom máximo do usuário
+        >
+          <Marker coordinate={localizacao} draggable />
+        </MapView>
       </View>
     </>
   );
